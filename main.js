a40005bf99b5a775630f947bb8e1c066cef1e075
Danielhbhhgdthg <96148202+Danielhbhhgdthg@users.noreply.github.com>
2022-06-29 18:17:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var canvas = new fabric.Canvas('mycanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromURL('BirthdayImage.jpg', function(Img) {
+		image = Img;
+		
+		image.scaleToWidth(700);
+		image.scaleToHeight(510);
+		image.set({
+		top:0,	
+		left:0	
+		});
+		canvas.add(image);
+	});
+	
+}
+
+function playSound(){
+	x.play();
+}
